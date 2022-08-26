06c8aa77dd7f00abf72f537ab34336a9b9618aec
Nedreha <Vadym.Nedreha@progress.com>
2022-08-26 10:29:42 +0300

    Added SpringWebHibernateAOP project example

---
+++ readme.md
@@ -0,0 +1,12 @@
+## Spring web example using Hibernate to DB connection.
+
+### In this project I use Hibernate for Spring framework.
+
+### In project were used next technologies:
+* Spring (Hibernate, MVC).
+* Maven
+* Database - Postgres
+* Server - Apache Tomcat
+* IDE - IntelliJ IDEA
+* JSP
+    
---
+++ src/main/java/com/vadim/spring_course_mvc_hibernate_aop/entity/Employee.java
@@ -0,0 +1,41 @@
+package com.vadim.spring_course_mvc_hibernate_aop.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "department")
+    private String department;
+
+    @Column(name = "salary")
+    private int salary;
+
+    public Employee(String name, String surname, String department, int salary) {
+        this.name = name;
+        this.surname = surname;
+        this.department = department;
+        this.salary = salary;
+    }
+}
